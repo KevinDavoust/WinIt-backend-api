e02ec3f05fa1f286850833de962fac8fec46bfc7
KevinDavoust <kevin.davoust@orange.fr>
2023-12-20 14:26:30 +0100

    initial commit

---
+++ src/test/java/com/cda/winit/WinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cda.winit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
