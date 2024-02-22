a4cd857855c094271d674ea61b2866be6db9ef2b
Rémi Meslin <remi.meslin@guarani.fr>
2024-02-22 16:35:27 +0100

    [FEATURE] Ajout interface et méthode création tournoi

--- src/main/java/com/cda/winit/tournament/domain/service/TournamentService.java
+++ src/main/java/com/cda/winit/tournament/domain/service/TournamentService.java
@@ -1,6 +1,9 @@
 package com.cda.winit.tournament.domain.service;
 
+import com.cda.winit.tournament.domain.dto.TournamentCreationDto;
 import com.cda.winit.tournament.domain.entity.Tournament;
+import com.cda.winit.tournament.domain.mappers.TournamentEntityMappers;
+import com.cda.winit.tournament.domain.service.interfaces.ITournamentService;
 import com.cda.winit.tournament.infrastructure.repository.TournamentRepository;
 import com.cda.winit.tournament.infrastructure.repository.exception.TournamentNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +13,20 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class TournamentService {
+public class TournamentService implements ITournamentService {
 
     private final TournamentRepository repository;
+    private final TournamentEntityMappers tournamentEntityMappers;
+
+    public Tournament createTournament(TournamentCreationDto newTournamentDto) throws Exception {
+        Tournament newTournament = tournamentEntityMappers.ToEntity(newTournamentDto);
+        return repository.save(newTournament);
+    }
 
     public List<Tournament> getAllTournament() {
         return repository.findAll();
     }
+
     public Tournament getOneTournament(Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new TournamentNotFoundException(id));
